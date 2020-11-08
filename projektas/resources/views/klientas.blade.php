faf5e73bee0d1cd3957a5a373b1711e3f1f7437d
Ricore <orentas.orentas20@gmail.com>
2020-11-08 21:46:35 +0200

    fix

--- projektas/resources/views/klientas.blade.php
+++ projektas/resources/views/klientas.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
     <p><a href="{{route('redaguoti_profili')}}">Redaguoti profilį</a></p>
-    <p><a href="{{route('pasalinti_profili')}}">Pašalinti profilį</a></p>
+    <p><a href="#">Pašalinti profilį</a></p>
     <p><a href="{{route('atsiliepimai')}}">Parašyti atsiliepimą</a></p>
 
 
