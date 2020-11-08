6b405f4146b7fb0fd3ac2e439617b2d34de9f385
NamelessSage <remyga39@gmail.com>
2020-11-08 15:13:53 +0200

    fixes

--- projektas/app/Http/Controllers/HomeController.php
+++ projektas/app/Http/Controllers/HomeController.php
@@ -32,7 +32,7 @@ class HomeController extends Controller
     public function krepselioPatvirtinimas()
     {
         return view('krepselioPatvirtinimas');
-
+    }
     public function krepselis()
     {
         return view('krepselis');
