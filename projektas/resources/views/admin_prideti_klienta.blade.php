c0c9ec80bfbc55dc08205482108041c3a58c8b7a
NamelessSage <remyga39@gmail.com>
2020-11-08 14:55:28 +0200

    navigation

---
+++ projektas/resources/views/admin_prideti_klienta.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.app')
+
+@section('content')
+    <p><a href="{{route('admin_klientas')}}">Patvirtinti</a></p>
+
+
+@endsection
--- projektas/routes/web.php
+++ projektas/routes/web.php
@@ -21,6 +21,16 @@ Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 Route::get('/krepselis', [App\Http\Controllers\HomeController::class, 'krepselis'])->name('krepselis');
-Route::get('/administracinis', [App\Http\Controllers\HomeController::class, 'administracinis'])->name('administracinis');
 Route::get('/klientas', [App\Http\Controllers\HomeController::class, 'klientas'])->name('klientas');
 Route::get('/konstravimo', [App\Http\Controllers\HomeController::class, 'konstravimo'])->name('konstravimo');
+
+//administracine dalis
+Route::get('/administracinis', [App\Http\Controllers\HomeController::class, 'administracinis'])->name('administracinis');
+Route::get('/administracinis/uzsakymai', [App\Http\Controllers\HomeController::class, 'admin_uzsakymai'])->name('admin_uzsakymai');
+Route::get('/administracinis/klientai', [App\Http\Controllers\HomeController::class, 'admin_klientai'])->name('admin_klientai');
+Route::get('/administracinis/naujas_klientas', [App\Http\Controllers\HomeController::class, 'admin_prideti_klienta'])->name('admin_naujas_klientas');
+Route::get('/administracinis/admin_klientas', [App\Http\Controllers\HomeController::class, 'admin_klientas'])->name('admin_klientas');
+Route::get('/administracinis/admin_redaguoti_klienta', [App\Http\Controllers\HomeController::class, 'admin_redaguoti_klienta'])->name('admin_redaguoti_klienta');
+
+Route::get('/administracinis/admin_pagalbos_sarasas', [App\Http\Controllers\HomeController::class, 'admin_pagalbos_sarasas'])->name('admin_pagalbos_sarasas');
+Route::get('/administracinis/admin_pokalbio_langas', [App\Http\Controllers\HomeController::class, 'admin_pokalbio_langas'])->name('admin_pokalbio_langas');
