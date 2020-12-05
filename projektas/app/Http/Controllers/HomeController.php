2caefc892bcccd05831b7dba84c6cca51bb4d6a3
Ricore <orentas.orentas20@gmail.com>
2020-12-05 20:19:20 +0200

    profile edit

--- projektas/app/Http/Controllers/HomeController.php
+++ projektas/app/Http/Controllers/HomeController.php
@@ -3,6 +3,9 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Http\Requests\UserUpdate;
+use Illuminate\Support\Facades\Auth;
+
 
 class HomeController extends Controller
 {
@@ -98,5 +101,13 @@ class HomeController extends Controller
     public function pagalba()
     {
         return view('pagalba');
+    }
+    public function redaguoti_profilipost(UserUpdate $request){
+        $user = Auth::user();
+        $user->name = $request['name'];
+        $user->username = $request['username'];
+        $user->email = $request['email'];
+        $user->save();
+        return back();
     }
 }
--- projektas/resources/views/redaguoti_profili.blade.php
+++ projektas/resources/views/redaguoti_profili.blade.php
@@ -1,6 +1,60 @@
 @extends('layouts.app')
 
 @section('content')
-
+    <div class="container">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="card-body">
+                        <div class="card-header bg-light">
+                            Profilio redagavimas
+                        </div>
+                        <form action="{{route('redaguoti_profilipost')}}" method="post">
+                            @csrf
+                        <div class="card-body">
+{{--                            <div class="col-md-4 mb-4">--}}
+{{--                                <div>Profilio informacija</div>--}}
+{{--                                <div class="text-muted small">Matoma visu</div>--}}
+{{--                            </div>--}}
+                            <div class="col-md-8">
+                                <div class="row">
+                                    <div class="col-md-6">
+                                        <div class="form-group">
+                                            <label class="form-control-label">Name</label>
+                                            <input name="name" class="form-control" value="{{Auth::user()->name}}">
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-md-8">
+                                <div class="row">
+                                    <div class="col-md-6">
+                                        <div class="form-group">
+                                            <label class="form-control-label">Username</label>
+                                            <input name="username" class="form-control" value="{{Auth::user()->username}}">
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-md-8">
+                                <div class="row">
+                                    <div class="col-md-6">
+                                        <div class="form-group">
+                                            <label class="form-control-label">Email</label>
+                                            <input name ="email" class="form-control" value="{{Auth::user()->email}}">
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="card-footer bg-light ">
+                                <button type="submit" class="btn btn-primary">Save Changes</button>
+                            </div>
+                        </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 
 @endsection
