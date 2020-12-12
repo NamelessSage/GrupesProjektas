e739e594db477437b6bc597b18a881d7acc7b3f5
Pofkinis <povkab@gmail.com>
2020-12-12 21:50:00 +0200

    styles added for cart

--- projektas/resources/views/cart.blade.php
+++ projektas/resources/views/cart.blade.php
@@ -3,29 +3,69 @@
 
 
 @section('content')
-    <h2>Prekių krepšelis</h2>
-    <div class="float-container">
+    <h2 class="cartTitle">Prekių krepšelis</h2>
+    <div class="">
 
         @foreach ($cart->cart_parts as $cart_part)
-            <table class="table">
+            <div class="modal fade bd-example-modal-lg" id="model-{{$cart_part->id}}" tabindex="-1" role="dialog" aria-labelledby="model-{{$cart_part->id}}" aria-hidden="true">
+                <div class="modal-dialog modal-lg">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <h5 class="modal-title"></h5>
+                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                <span aria-hidden="true">&times;</span>
+                            </button>
+                        </div>
+                        <div class="modal-body">
+                            <div class="image">
+                                <img src="{{ $cart_part->part->image }}" alt="jack">
+                            </div>
+
+                            <div class="card-content">
+
+                                <div class="title">
+                                    <h3>{{  $cart_part->part->name }}</h3>
+                                </div>
+
+                                <div class="price">
+                                    <h4>{{  $cart_part->part->price }}€</h4>
+                                </div>
+
+                                <div class="description">
+                                    <p>{{  $cart_part->part->about }}</p>
+                                </div>
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <table class="table container">
                 <tbody>
                 <tr>
-                    <td><img src="{{ $cart_part->part->image }}" alt="{{ $cart_part->image }}"></td>
+                    <td><img width="150px" src="{{ $cart_part->part->image }}" alt="{{ $cart_part->image }}"></td>
                     <td><h3>{{ $cart_part->part->name }}</h3></td>
+                    <td>
+                        <button class="btn btn-primary" data-toggle="modal" data-target="#model-{{$cart_part->id}}" type="button" name="button">Preview</button>
+                    </td>
+
+                    <td><h4>{{ $cart_part->total_price }}€</h4></td>
                     <form method="post" action="{{ route('changeQuantity', $cart_part)}}">
                         @csrf
                         <td>
-                            <input type="number" name="qty" value="{{ $cart_part->quantity }}" min="0" >
-                            <button type="submit" class="btn btn-primary">Save</button>
-                        </td>
+                            <div class="qty">
+                                <input type="number" name="qty" value="{{ $cart_part->quantity }}" min="0" >
+                                <button type="submit" class="btn btn-primary">Save</button>
+                            </div>
 
+
+                        </td>
                     </form>
-                    <td><h4>{{ $cart_part->total_price }}€</h4></td>
                     <td>
                         <form class="" action="{{ route('destroyCartPart', $cart_part->id) }}" method="get">
                             @csrf
                             @method('DELETE')
-                            <button type="submit" class="btn"><i class="fas fa-trash-alt"></i>Delete</button>
+                            <button type="submit" class="btn btn-danger"><i class="fas fa-trash-alt"></i>Remove</button>
                         </form>
                     </td>
                 </tr>
@@ -36,23 +76,29 @@
 
 
                 @endforeach
+    <hr class="sepLine">
+        <div class="container">
+            <form action="{{ route('createDiscount', $cart) }}" method="get">
+                <label for="discount">Įveskite nuolaidos kodą: </label>
+                <div class="discount">
+                    <input class="form-control" type="text" name="discount" id="">
+                    <button class="btn btn-dark" type="submit">Patvirtinti</button>
+                </div>
 
-        <form action="{{ route('createDiscount', $cart) }}" method="get">
-            <label for="discount">Įveskite nuolaidos kodą</label>
-            <input type="text" name="discount" id="">
-            <button type="submit">Patvirtinti</button>
-        </form>
+            </form>
 
-        <div class="check-out">
+            <div class="checkout">
 
-            <div class="total-price">
-                <h4>Total price: <b>{{ $cart->getTotalPrice()}}</b></h4>
-            </div>
+                <div class="total-price">
+                    <h4>Total price: <b>{{ $cart->getTotalPrice()}}</b></h4>
+                </div>
 
-            <div class="buy-btn">
-                <a href="{{ route('makeOrder', $cart) }}"> <button type="button" name="button" class="btn btn-success">CHECK-OUT</button> </a>
+                <div class="buy-btn">
+                    <a href="{{ route('makeOrder', $cart) }}"> <button type="button" name="button" class="btn btn-success">CHECK-OUT</button> </a>
+                </div>
             </div>
         </div>
+
     </div>
 
 @endsection
