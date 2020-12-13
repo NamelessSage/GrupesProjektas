cadd63693d1937936b4e9c1bc3988b433e7070d2
NamelessSage <remyga39@gmail.com>
2020-12-13 19:05:37 +0200

    Front

--- projektas/resources/views/admin_klientai.blade.php
+++ projektas/resources/views/admin_klientai.blade.php
@@ -5,18 +5,18 @@
         <div class="row justify-content-center">
             <div class="col-md-15">
                 <div class="card">
-                    <div class="card-header">{{ __('Klientu sarasas') }}</div>
+                    <div class="card-header">{{ __('Klientų sąrašas') }}</div>
                     <div class="card-body">
                         <table class="table">
                             <tr>
                                 <th>Klientas</th>
-                                <th>perziureti</th>
+                                <th>Peržiūrėti</th>
                             </tr>
                             @foreach($klientai as $klientas)
                                 <tr>
                                     <td>{{ $klientas->vardas  }} {{ $klientas->pavarde  }}</td>
                                     <td>
-                                        <a href="{{route('admin_klientas', $klientas->id)}}"><button class="btn btn-primary" style="margin: 5px">Perziureti</button></a>
+                                        <a href="{{route('admin_klientas', $klientas->id)}}"><button class="btn btn-primary" style="margin: 5px">Peržiūrėti</button></a>
                                     </td>
                                 </tr>
                             @endforeach
